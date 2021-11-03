df0962b10c6a196bd3e3ce3f0844ac14970c1077
lapinRagnar <lapinragnar@gmail.com>
2021-11-04 00:48:15 +0100

    créer url dans hotel_pour_animal et inclure dans l'urls de notre app_2

--- app_2/urls.py
+++ app_2/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hotel_pour_animal', include('hotel_pour_animal.urls')),
+
 ]
